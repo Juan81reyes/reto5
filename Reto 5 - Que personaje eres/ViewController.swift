ba39b26e5a622dd7040edb0cab5d617a80fcbf22
Angel Ruiz <angel.ruiz@forteinnovation.mx>
2020-07-20 13:08:40 -0500

    Initial Commit

---
+++ Reto 5 - Que personaje eres/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Reto 5 - Que personaje eres
+//
+//  Created by Forte Apps on 20/07/20.
+//  Copyright © 2020 UVEG. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
